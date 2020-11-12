9d4a13fa32e0772d2d9cb64f1226d287c3236c69
Kelvin Conrad <klvnconrad@gmail.com>
2020-11-12 21:45:19 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+//Initiating Feather Icons
+feather.replace()
+
+//Initializing Owl Corousel
+$('.clients-corousel').owlCarousel({
+    loop:true,
+    margin:50,
+    autoplay:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.testimonial-corousel').owlCarousel({
+    loop:true,
+    margin:25,
+    autoplay:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
