5f82d74f4389bf72a8384cf573742d42da3647d6
Kelvin Conrad <klvnconrad@gmail.com>
2020-11-13 17:10:24 +0300

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -41,3 +41,56 @@ $('.testimonial-corousel').owlCarousel({
     }
 })
 
+// Smooth Scroll
+let magicScroll = new MagicScroll({
+    target: document.querySelector(".scrollableElement"),
+    speed: 80,
+    smooth: 12,
+    current: 0 // position
+});
+
+
+//Scroll Reveal
+var rafId = null;
+var delay = 200;
+var lTime = 0;
+
+function scroll() {
+  var scrollTop = $(window).scrollTop();
+  var height = $(window).height()
+  var visibleTop = scrollTop + height;
+  $('.reveal').each(function() {
+    var $t = $(this);
+    if ($t.hasClass('reveal_visible')) { return; }
+    var top = $t.offset().top;
+    if (top <= visibleTop) {
+      if (top + $t.height() < scrollTop) {
+        $t.removeClass('reveal_pending').addClass('reveal_visible');
+      } else {
+        $t.addClass('reveal_pending');
+        if (!rafId) requestAnimationFrame(reveal);  
+      }
+    }
+  });
+}
+function reveal() {
+  rafId = null;
+  var now = performance.now();
+  
+  if (now - lTime > delay) {
+    lTime = now;
+    var $ts = $('.reveal_pending');
+    $($ts.get(0)).removeClass('reveal_pending').addClass('reveal_visible');  
+  }
+  
+  
+  if ($('.reveal_pending').length >= 1) rafId = requestAnimationFrame(reveal);
+}
+
+$(scroll);
+$(window).scroll(scroll);
+$(window).click(function() {
+  lTime = performance.now() + 500;
+  var top = $(window).scrollTop();
+  $(window).scrollTop(top === 0 ? 1 : top-1);
+});
